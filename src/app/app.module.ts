8bf5f1e95f2b9df0cadbc923553229e76ff73cf5
Rahool <rahooolpatil.rp@gmail.com>
2020-08-02 01:32:20 +0530

    UI changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AgGridModule } from 'ag-grid-angular';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgGridModule.withComponents([]),
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
